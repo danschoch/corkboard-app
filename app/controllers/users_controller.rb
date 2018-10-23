9a86a5eb45c82f4fb3520aa8665ef6b11d0d3d18
danschoch <daniel.alexander.schoch@gmail.com>
2018-10-23 10:25:05 -0700

    edit views and controller actions for signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,55 @@ class UsersController < ApplicationController
     get '/home' do
         redirect to '/notebooks'
     end
+
+    get '/signup' do
+        if !logged_in?
+            erb :'sessions/signup'
+        else
+            redirect to '/notebooks'
+        end
+    end
+
+    post '/signup' do
+        user_emails = User.all.collect {|user| user.email}
+        usernames = User.all.collect {|user| user.username}
+
+        if user_emails.include?(params[:email]) || usernames.include?(params[:username]) || params[:username] == "" || params[:email] == "" || params[:password] == ""
+            redirect to '/signup'
+        else
+            @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+            session[:user_id] = @user.id
+            redirect to '/notebooks'
+        end
+    end
+
+=begin    
+    get '/login' do
+        if logged_in?
+            redirect to '/tweets'
+        else
+            erb :'users/login'
+        end
+    end
+
+    post '/login' do
+        user = User.find_by(:username => params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect to '/tweets'
+        else
+            redirect to '/signup'
+        end
+    end
+
+    get '/logout' do
+        session.clear
+        redirect "/login"
+    end
+
+    get '/users/:slug' do
+        @user = User.find_by_slug(params[:slug])
+        erb :'users/show'
+    end
+=end
 end
