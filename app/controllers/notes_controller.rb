dea0f86c4d1b9a36b75771d66f9418e7115bc200
danschoch <daniel.alexander.schoch@gmail.com>
2018-10-23 14:58:18 -0700

    add edit note views and controller actions - does not delete note from old notebook

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -23,6 +23,34 @@ class NotesController < ApplicationController
         end
     end
 
+    get '/notes/:id/edit' do
+        if logged_in?
+            @note = Note.find_by_id(params[:id])
+            erb :'notes/edit_note'
+        else
+            redirect to '/login'
+        end
+    end
+
+    patch '/notes/:id' do
+        if logged_in?
+            if params[:note_title] !=""
+                @note = Note.find_by_id(params[:id])
+                @current_notebook = Notebook.find_by_id(params[:notebook_id])
+
+                @note.update(title: params[:note_title], content: params[:note_content])
+                @current_notebook.notes<< @note if !@current_notebook.notes.include?(@note)
+
+                @note.save
+                redirect to "/notebooks/#{@current_notebook.id}"
+            else
+                redirect to '/notebooks'
+            end
+        else
+            redirect to '/login'
+        end
+    end
+
     delete '/notes/:id/delete' do
         if logged_in?
             @note = Note.find_by_id(params[:id])
