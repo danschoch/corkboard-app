c8a62905136d6634d222c16bf38018989b1df8e2
danschoch <daniel.alexander.schoch@gmail.com>
2018-10-23 14:36:29 -0700

    add edit controller actions and views for notebooks

--- app/controllers/notebooks_controller.rb
+++ app/controllers/notebooks_controller.rb
@@ -42,6 +42,30 @@ class NotebooksController < ApplicationController
         end
     end
 
+    get '/notebooks/:id/edit' do
+        if logged_in?
+            @notebook = Notebook.find_by_id(params[:id])
+            erb :'notebooks/edit'
+        else
+            redirect to '/login'
+        end
+    end
+
+    patch '/notebooks/:id' do
+        if logged_in?
+            if params[:notebook_name] !=""
+                @notebook = Notebook.find_by_id(params[:id])
+                @notebook.update(name: params[:notebook_name], description: params[:notebook_description])
+                @notebook.save
+                redirect to "/notebooks/#{@notebook.id}"
+            else
+                redirect to '/notebooks'
+            end
+        else
+            redirect to '/login'
+        end
+    end
+
     delete '/notebooks/:id/delete' do
         if logged_in?
             @notebook = Notebook.find_by_id(params[:id])
