6c8685638d88315f35cb4798dd49c476ae61b7aa
danschoch <daniel.alexander.schoch@gmail.com>
2018-10-25 16:12:09 -0700

    add control flow for edit user patch action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,6 +42,64 @@ class UsersController < ApplicationController
         end
     end 
 
+    get '/users/:id/edit' do
+        if logged_in?
+            @user = User.find_by_id(params[:id])
+            if @user.id == current_user.id
+                erb :'users/edit_user'
+            else
+                redirect to '/logout'
+            end
+        else
+            redirect to '/login'
+        end
+    end
+
+    patch '/users/:id' do
+        if logged_in?
+            @user = User.find_by_id(params[:id])
+            if @user.id == current_user.id
+                user_emails = User.all.collect {|user| user.email}
+                usernames = User.all.collect {|user| user.username}
+                if params[:username] == "" || params[:email] == "" || params[:password] == "" || params[:password] != params[:confirm_password]
+                    redirect to "/notebooks/users/#{@user.id}/edit"
+                elsif user_emails.include?(params[:email]) && usernames.include?(params[:username])
+                    if @user.email == params[:email] && @user.username == params[:username]
+                        @user.update(password: params[:password])
+                        @user.save
+                        redirect to '/notebooks'
+                    else
+                        redirect to "/notebooks/users/#{@user.id}/edit"
+                    end
+                elsif user_emails.include?(params[:email])
+                    if @user.email == params[:email]
+                        @user.update(username: params[:username], password: params[:password])
+                        @user.save
+                        redirect to '/notebooks'
+                    else
+                        redirect to "/notebooks/users/#{@user.id}/edit"
+                    end
+                elsif usernames.include?(params[:username])
+                    if @user.username = params[:username]
+                        @user.update(email: params[:email], password: params[:password])
+                        @user.save
+                        redirect to '/notebooks'
+                    else
+                        redirect to "/notebooks/users/#{@user.id}/edit"
+                    end
+                else
+                    @user.update(username: params[:username], email: params[:email], password: params[:password])
+                    @user.save
+                    redirect to '/notebooks'
+                end
+            else
+                redirect to '/logout'
+            end
+        else
+            redirect to '/login'
+        end
+    end
+
     get '/logout' do
         session.clear
         redirect to '/'
